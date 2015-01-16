b3caaff9aa9210d5de2dc493fc0ce94c53b29d36
John Richardson <johnrichardson2387@gmail.com>
2015-01-16 08:42:04 +0530

    dot net and php added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   end
 
   def techs
-
+    techarr = ['php', 'dotnet', 'testing']
+    @techname = params[:name]
+    unless techarr.include?(@techname)
+      redirect to root_path
+    end
   end
 end
