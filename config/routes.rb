8d88c05149d9e46f1f437f82c12b05191480257b
John Richardson <johnrichardson2387@gmail.com>
2015-01-17 10:01:08 +0530

    added new routes and menu

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get 'technologies/:name' => 'home#techs'
+  get 'technologies/:name' => 'home#techs', as: :technologies
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
